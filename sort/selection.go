7dea92914835fee81bd544661cd23921fcc5dec6
Canux CHENG <canuxcheng@gmail.com>
2018-06-25 22:49:23 +0800

    add selection sort.

--- sort/selection.go
+++ sort/selection.go
@@ -1,1 +1,16 @@
 package sort
+
+func Selection(seq []int64) []int64 {
+    for i := 0; i < len(seq) - 1; i++ {
+    	minIndex := i
+    	for j := i+1; j < len(seq); j++ {
+    		if seq[j] < seq[minIndex] {
+    			minIndex = j
+			}
+		}
+		if minIndex != i {
+			seq[i], seq[minIndex] = seq[minIndex], seq[i]
+		}
+	}
+	return seq
+}
--- sort/selection_test.go
+++ sort/selection_test.go
@@ -1,1 +1,16 @@
 package sort
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestSelection(t *testing.T) {
+	seq := []int64{23, 78, -25, 82, 0, 589}
+	if res := Selection(seq); res[0] != -25 && res[5] != 589 {
+		fmt.Println(res)
+		t.Error("Selection sort failed.")
+	} else {
+		fmt.Println(res)
+	}
+}
