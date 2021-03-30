9230d1154087119959bbcec10b4f0de91df80015
Canux <canux.cheng@arm.com>
2021-03-29 23:39:04 -0400

    bugfix

--- search/search_test.go
+++ search/search_test.go
@@ -16,7 +16,7 @@ func TestBinary(t *testing.T) {
 
 func TestLinear(t *testing.T) {
 	seq1 := []int64{23, 78, -25, 82, 0, 589}
-	if res := SequentialSearch(seq1, -25); res == -1 {
+	if res := LinearSearch(seq1, -25); res == -1 {
 		t.Error("Failed: search -25 failed.")
 	} else {
 		fmt.Println(res)
